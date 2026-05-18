d9f9a814c8c190da1f08c2ab35d3b4230e1fbdcb
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2026-05-18 11:16:26 +0600

    the microsoft clarity has been added

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,6 +103,17 @@ export default function RootLayout({
             gtag('config', 'G-N1FD35QQ75');
           `}
         </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {\`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wsvl3n571z");
+          \`}
+        </Script>
       </body>
     </html>
   );
